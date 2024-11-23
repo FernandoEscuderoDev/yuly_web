f589fade45e95786a9866a4cd9237abaa8591197
FernDev <fernando.andres.escudero@gmail.com>
2024-11-23 06:30:22 -0300

    solucion a Sanity.config.js

---
+++ sanity.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import {visionTool} from '@sanity/vision'
+
+import { schema } from "./src/sanity/schemaTypes";
+import { unsplashImageAsset } from "sanity-plugin-asset-source-unsplash";
+
+export default defineConfig({
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
+  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+  plugins: [structureTool(), visionTool(),unsplashImageAsset()],
+  schema,
+});
