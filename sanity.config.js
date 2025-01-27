88647814e3360103edc4ed0a1d2820fe915872a1
FernDev <fernando.andres.escudero@gmail.com>
2025-01-27 01:40:59 -0300

    quita vision

--- sanity.config.js
+++ sanity.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-import { visionTool } from "@sanity/vision";
 import { schema } from "./src/sanity/schemaTypes";
 import { unsplashImageAsset } from "sanity-plugin-asset-source-unsplash";
 import { media } from "sanity-plugin-media";
@@ -11,7 +10,7 @@ export default defineConfig({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET,
 
-  plugins: [structureTool(), visionTool(), unsplashImageAsset(), media()],
+  plugins: [structureTool(), unsplashImageAsset(), media()],
 
   schema: schema,
 
